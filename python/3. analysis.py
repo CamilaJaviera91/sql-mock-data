89a5f48e7cca635f86bf8f26a133235b6da20dec
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-04-06 19:03:35 -0400

    feat(analysis): create function so we can see the ages of the employees

---
+++ python/3. analysis.py
@@ -0,0 +1,34 @@
+from pyspark.sql import functions as F
+from pyspark.sql import SparkSession
+import logging
+
+def mean_age():
+
+    df = spark.read.option("header", "true").option("inferSchema", "true") \
+    .csv("./data/*.csv")
+
+    df_age = df.withColumn(
+        "age",
+        F.floor(F.months_between(F.current_date(), F.col("date_birth")) / 12)
+    )
+
+    df_age.select("age").summary("count", "min", "max", "mean").show()
+
+
+
+if __name__ == "__main__":
+    # Create a Spark session
+    spark = SparkSession.builder.appName("Pyspark").getOrCreate()
+
+    # Set logging level to 'ERROR' to minimize logs
+    spark.sparkContext.setLogLevel("ERROR")
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        handlers=[logging.StreamHandler()])
+    
+    logger = logging.getLogger(__name__)
+
+    # Run the function if the script is executed directly
+
+    mean_age()
