6ffcd4063462c009ab2fb90531b0fbc834c3ddc2
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-04-15 18:51:53 -0400

    feat(show): create function to show function by age

--- sql/phyton/show_results.py
+++ sql/phyton/show_results.py
@@ -24,10 +24,24 @@ def plot_by_department():
         plt.tight_layout()
         plt.show()
 
+def plot_by_age():
+    df = by_department()
+    if df is not None:
+        plt.figure(figsize=(12, 6))
+        sns.barplot(x='active_employees', y='department', data=df, palette='Purples_d')
+        plt.title('Active Employees by Department')
+        plt.xlabel('Active Employees')
+        plt.ylabel('Departament')
+        plt.tight_layout()
+        plt.show()
+
 def main():
     print("=== Active Employees by City ===")
     plot_by_city()
+    print("=== Active Employees by Department ===")
     plot_by_department()
+    print("=== Active Employees by Age ===")
+    plot_by_age()
 
 if __name__ == "__main__":
     main()
